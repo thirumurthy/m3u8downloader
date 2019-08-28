6f5eb6aeaeeba5c9c1ea372ab5b1adb401c1a52f
Thirumurthy <91thiru@gmail.com>
2019-08-28 07:53:02 +0530

    Chunk added

--- Web/start.py
+++ Web/start.py
@@ -16,6 +16,10 @@ def home():
 @app.route('/airtel/<name>')  
 def playAirtel(name): 
     ptv = airteltv[name]
+    data =requests.get(ptv, headers={"content-type":"text"}, stream=True)
+    for chunk in data.iter_content(chunk_size=512):
+	if chunk:
+	   f.write(chunk)
     return "hello,"+name;  
 
 
